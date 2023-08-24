8c30464970772b99671f852acbe84e0ce8874e3c
davegerim <davegerim@gmail.com>
2023-08-24 13:08:39 +0300

    swagger update

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { json, urlencoded } from 'body-parser';
 import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist/swagger-module';
 
 async function bootstrap() {
   const cors = require('cors');
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cors({
-      orgin: 'http://localhost:3000',
-    }),
-  );
+
+  const config = new DocumentBuilder()
+    .setTitle('zuret tourism example')
+    .setDescription('a list of zuret tourism api')
+    .setVersion('1.0')
+    .addTag('tourism app')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.use(cors({ origin: 'http://localhost:3000' }));
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ limit: '50mb', extended: true }));
+
   await app.listen(3000);
 }
 bootstrap();
